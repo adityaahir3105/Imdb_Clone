9b1cf3c537b20e13069204427197215b164220e9
ahiraditya93@gmail.com <ahiraditya31@gmail.com>
2022-05-05 15:46:12 +0530

    first version

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "df78d688";
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>Created By Aditya Ahir</div>
+    </div>
+  );
+};
+
+export default Footer;
